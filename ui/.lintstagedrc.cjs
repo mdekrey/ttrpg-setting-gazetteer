3005e4f790068fded3cefa0b37be33227d080474
Matt DeKrey <mattdekrey@gmail.com>
2023-01-31 06:52:05 -0600

    Add typecheck to lint steps

--- ui/.lintstagedrc.cjs
+++ ui/.lintstagedrc.cjs
@@ -1,15 +1,18 @@
-const path = require('path')
+const path = require('path');
 
 const buildEslintCommand = (filenames) =>
 	`eslint --fix ${filenames
 		.map((f) => path.relative(process.cwd(), f))
-		.join(' ')}`
+		.join(' ')}`;
 
 const buildPrettierCommand = (filenames) =>
 	`prettier --write ${filenames
 		.map((f) => path.relative(process.cwd(), f))
-		.join(' ')}`
+		.join(' ')}`;
+
+const buildTypecheckCommand = (filenames) => `npm run typecheck`;
 
 module.exports = {
 	'*.{js,jsx,ts,tsx,astro,md,mdx}': [buildEslintCommand, buildPrettierCommand],
+	'*.{ts,tsx,astro,mdx}': [buildTypecheckCommand],
 };
